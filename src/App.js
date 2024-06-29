e21b54c81872fd9e39892c63001e37d0a26d6f10
xchip-in <mail@xchip.in>
2024-06-29 17:51:27 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://public.xchip.in/logo/logo.png" className="App-logo" alt="logo" />
+          <h3>We Engineered the X Chip.</h3>
+      </header>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Footer.css';
+import { FaXTwitter, FaLinkedin, FaInstagram, FaEnvelope } from 'react-icons/fa6';
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="social-media-links">
+          <a href="mailto:mail@xchip.in" target="_blank" rel="noopener noreferrer" className="social-media-link">
+            <FaEnvelope />
+          </a>
+          <a href="https://x.com/xchip_in/" target="_blank" rel="noopener noreferrer" className="social-media-link">
+            <FaXTwitter />
+          </a>
+          <a href="https://www.linkedin.com/company/xchip-in/" target="_blank" rel="noopener noreferrer" className="social-media-link">
+            <FaLinkedin />
+          </a>
+          <a href="https://instagram.com/xchip.in" target="_blank" rel="noopener noreferrer" className="social-media-link">
+            <FaInstagram />
+          </a>
+        </div>
+        <div className="footer-text">
+          © {currentYear} <a href="https://www.xchip.in" target="_blank" rel="noopener noreferrer">xchip.in</a>. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
