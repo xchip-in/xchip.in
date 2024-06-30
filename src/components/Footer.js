123dacdb70a01537fec16b3867cc8419ae54987a
xchip-in <mail@xchip.in>
2024-06-30 08:50:40 +0530

    youtube

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
-import { FaXTwitter, FaLinkedin, FaInstagram, FaEnvelope } from 'react-icons/fa6';
+import { FaYoutube, FaXTwitter, FaLinkedin, FaInstagram, FaEnvelope } from 'react-icons/fa6';
 
 function Footer() {
   const currentYear = new Date().getFullYear();
@@ -12,14 +12,17 @@ function Footer() {
           <a href="mailto:mail@xchip.in" target="_blank" rel="noopener noreferrer" className="social-media-link">
             <FaEnvelope />
           </a>
-          <a href="https://x.com/xchip_in/" target="_blank" rel="noopener noreferrer" className="social-media-link">
-            <FaXTwitter />
-          </a>
           <a href="https://www.linkedin.com/company/xchip-in/" target="_blank" rel="noopener noreferrer" className="social-media-link">
             <FaLinkedin />
           </a>
-          <a href="https://instagram.com/xchip.in" target="_blank" rel="noopener noreferrer" className="social-media-link">
+          <a href="https://x.com/xchip_in/" target="_blank" rel="noopener noreferrer" className="social-media-link">
+            <FaXTwitter />
+          </a>
+          <a href="https://www.instagram.com/xchip.in" target="_blank" rel="noopener noreferrer" className="social-media-link">
             <FaInstagram />
+          </a>
+          <a href="https://www.youtube.com/xchip-in" target="_blank" rel="noopener noreferrer" className="social-media-link">
+            <FaYoutube />
           </a>
         </div>
         <div className="footer-text">
